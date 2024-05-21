3dd5b85b920a7a2463b372a86af22d5c882f0fa1
rart <royart@me.com>
2024-05-21 14:47:44 +0200

    Updates - Fix build for assets loaded by JS modules with relative URLs so it works with Studio's Plugin File API - Upgrade/update to CrafterCMS 4.1.4 - Other dependency updates

--- examples/app-external-sources/vite.config.ts
+++ examples/app-external-sources/vite.config.ts
@@ -2,6 +2,10 @@ import react from '@vitejs/plugin-react';
 import replace from '@rollup/plugin-replace';
 import { defineConfig, loadEnv } from 'vite';
 import { createHtmlPlugin } from 'vite-plugin-html';
+import copy from 'rollup-plugin-copy';
+
+// Switch this accordingly
+const useExternalSources = true;
 
 const proxyConfig = {
   target: 'http://localhost:8080',
@@ -13,10 +17,16 @@ const proxyConfig = {
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.');
   return {
-    // The value of `base` should be whatever your external host address is.
-    base: mode === 'development' ? '/studio' : 'http://localhost:3000',
-    // If you're NOT using an external host, you can use the following line instead. Also need to uncomment the plugin code below (on the `plugins` section).
-    // base: mode === 'development' ? '/studio' : '/PLUGIN_FILE_URL/',
+    base:
+      mode === 'development'
+        ? // In development, we need this so that the authentication from localhost:8080 falls through to our localhost:{devServerPort}.
+          '/studio'
+        : useExternalSources
+        ? // The value of `base` should be whatever your external host address is.
+          'http://localhost:3000'
+        : // If you're NOT using an external host, you can use the following line instead, which
+          // works together with the little `replacePluginFileUrl` rollup plugin (invoked and declared below).
+          '/PLUGIN_FILE_URL/',
     server: {
       port: 3000,
       proxy: {
@@ -36,38 +46,63 @@ export default defineConfig(({ mode }) => {
           'process.env.PUBLIC_URL': JSON.stringify(env.VITE_APP_PUBLIC_URL)
         }
       }),
-      createHtmlPlugin({
-        entry: 'src/main.tsx',
-        minify: false
-      }),
-      // region Plugin File Url Replace Plugin
-      // If you're NOT using an external host, you can use the following plugin together with the suggested `base` property above.
-      // Make sure to replace the placeholders on the `replace` function with your actual values.
-      /* (function () {
-        const replace = (code) => code.replace(
-          /\/PLUGIN_FILE_URL\//g,
-          // Replace placeholders here:
-          '/studio/1/plugin/file?siteId=YOUR_SITE&type=YOUR_TYPE&name=YOUR_NAME&pluginId=YOUR_PLUGIN_ID&filename='
-        );
-        return {
-          name: 'ReplaceCrafterCMSPluginFileUrl',
-          generateBundle(_, bundle) {
-            for (const fileName in bundle) {
-              const chunk = bundle[fileName];
-              if (chunk.type === 'chunk') {
-                chunk.code = replace(chunk.code);
-              }
-            }
-          },
-          transformIndexHtml(html) {
-            return replace(html);
-          }
-        };
-      })() */
-      // endregion
+      createHtmlPlugin({ entry: 'src/main.tsx', minify: false }),
+      useExternalSources
+        ? // If you're using external sources, you'd deploy only `index.html` to your CrafterCMS project, and serve the rest from your external server.
+          copy({
+            hook: 'closeBundle',
+            // Once your index is at this target dest, use the `crafter-cli copy-plygin` or `/studio/api/2/marketplace/copy` API to deploy.
+            targets: [
+              { src: './dist/index.html', dest: '../../authoring/static-assets/plugins/org/craftercms/examples/awes' }
+            ]
+          })
+        : // If you're NOT using an external host, you can use this plugin with the suggested `base` property above.
+          // Make sure to replace the arguments with the values that correspond to your environment.
+          replacePluginFileUrl({
+            siteId: 'editorial',
+            type: 'examples',
+            name: 'awes',
+            pluginId: 'org.craftercms'
+          })
     ],
     build: {
-      minify: false
+      minify: false,
+      emptyOutDir: true,
+      outDir: useExternalSources
+        ? // If you're using external sources, you'd need to deploy `dist` to your server. This example is served from right from `dist` by invoking `yarn serve`.
+          'dist'
+        : // If not using external sources, the entire build gets deployed to your CrafterCMS project. Using the path below, will put the build where you can simply
+          // invoke/use the `crafter-cli copy-plygin` or `/studio/api/2/marketplace/copy` API to deploy.
+          '../../authoring/static-assets/plugins/org/craftercms/examples/awes'
     }
   };
 });
+
+interface ReplacePluginFileUrlArgs {
+  siteId: string;
+  type: string;
+  name: string;
+  pluginId: string;
+}
+
+function replacePluginFileUrl({ siteId, type, name, pluginId }: ReplacePluginFileUrlArgs) {
+  const replacement = `/studio/1/plugin/file?siteId=${siteId}&type=${type}&name=${name}&pluginId=${pluginId}&filename=`;
+  const replacement2 = `${replacement}assets/`;
+  const replace = (code: string) => code.replace(/\/PLUGIN_FILE_URL\//g, replacement);
+  return {
+    name: 'Replace Plugin File Url',
+    generateBundle(_, bundle) {
+      for (const fileName in bundle) {
+        const chunk = bundle[fileName];
+        if (chunk.type === 'chunk') {
+          chunk.code = replace(chunk.code).replace(/import.*?\(?(["'])((\.{1,2}\/).*)\1/g, (match) => {
+            return match.replace(/\.{1,2}\//, replacement2);
+          });
+        }
+      }
+    },
+    transformIndexHtml(html) {
+      return replace(html);
+    }
+  };
+}
